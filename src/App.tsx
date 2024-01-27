82dc1f54037fc6e83691adc13fb147db715edc05
Simon Edelmann <admin@simonedelmann.de>
2024-01-27 16:39:25 +0100

    Example with Catalyst

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Listbox, ListboxOption, ListboxLabel } from './Listbox'
+
 function App() {
   return (
-    <div className="text-red-300">
-      Hello World
+    <div className="min-h-screen bg-gray-300 flex items-center justify-center">
+      <div className="bg-white p-8 relative">
+      <Listbox name="status" defaultValue="active">
+        <ListboxOption value="active">
+          <ListboxLabel>Active</ListboxLabel>
+        </ListboxOption>
+        <ListboxOption value="paused">
+          <ListboxLabel>Paused</ListboxLabel>
+        </ListboxOption>
+        <ListboxOption value="delayed">
+          <ListboxLabel>Delayed</ListboxLabel>
+        </ListboxOption>
+        <ListboxOption value="canceled">
+          <ListboxLabel>Canceled</ListboxLabel>
+        </ListboxOption>
+      </Listbox>
+      </div>
     </div>
   )
 }
